a8850efdff6bdd47653e37101304bc6f162063a9
Ivan.Lopez <talivan.lo@gmail.com>
2024-10-23 08:50:06 -0600

    Actualizando ejemplos...3er dia.

--- Capitulo1/EjercicioCapas/EjercicioCapas/Business/Entities/Autor.cs
+++ Capitulo1/EjercicioCapas/EjercicioCapas/Business/Entities/Autor.cs
@@ -7,6 +7,7 @@ namespace EjercicioCapas.Business.Entities
         public int Id { get; set; } 
         public string Name { get; set; }
         public string Nationality { get; set; }
+
         [JsonIgnore]
         public ICollection<Book> Books { get; set; }
         public Autor()
---
+++ Capitulo1/MVVMApp/MVVMApp/ViewModel/ClockViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Timers;
+
+namespace MVVMApp.ViewModel
+{
+    public class ClockViewModel
+    {
+        public System.Timers.Timer timer { get; set; }
+        public string CurrentTime { get; set; } = DateTime.Now.ToString("hh:mm:ss");
+
+        public string Name { get; set; }
+
+        public ClockViewModel()
+        {
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += UpdateTime;
+            timer.Start();
+        }
+
+        public void UpdateTime(object sender, ElapsedEventArgs args)
+        {
+            CurrentTime = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
